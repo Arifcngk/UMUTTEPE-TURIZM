afff4057677410f8a9490dcca877c09b2a8cbd63
Mehmet Sezer <mehmetszr05@gmail.com>
2024-03-01 23:47:00 +0300

    Update: Login Popup removed

--- sign_up.php
+++ sign_up.php
@@ -10,6 +10,7 @@
       rel="stylesheet"
       href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
     />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Umuttepe Turizm-Kayıt Ol</title>
   </head>
   <body>
@@ -76,7 +77,7 @@
                 <span><i aria-hidden="true" class="fa fa-lock"></i></span>
                 <input
                   type="password"
-                  name="password"
+                  name="repassword"
                   placeholder="Parola Tekrar"
                   required
                 />
