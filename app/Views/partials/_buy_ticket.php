42576df41a377c909d9c511d83aacfd45bdeeea6
Mehmet Sezer <mehmetszr05@gmail.com>
2024-03-05 19:13:03 +0300

    route buttons enabled

--- app/Views/partials/_buy_ticket.php
+++ app/Views/partials/_buy_ticket.php
@@ -7,6 +7,29 @@
         </div>
         <div id="tab1">
             <div class="submit-form">
+                <div class="btn-group pt-2 pb-2" role="group" aria-label="Route type switch">
+                    <button type="button" class="btn btn-secondary" onclick="activateButton(1)">Tek Yön</button>
+                    <button type="button" class="btn btn-secondary" onclick="activateButton(2)">Gidiş-Dönüş</button>
+                </div>
+
+                <script>
+                    function activateButton(buttonNumber) {
+                        // Tüm butonları pasif hale getir
+                        var buttons = document.querySelectorAll('.btn-group .btn');
+                        buttons.forEach(function(button) {
+                            button.classList.remove('active');
+                        });
+
+                        // Tıklanan butonu aktif hale getir
+                        var clickedButton = document.querySelector('.btn-group .btn:nth-child(' + buttonNumber + ')');
+                        clickedButton.classList.add('active');
+
+
+                        var returnDate = document.querySelector('#return');
+
+                        returnDate.disabled = (buttonNumber == 1) ? true : false; 
+                    }
+                </script>
                 <h4>Sefer Bilgilerinizi Giriniz</h4>
                 <form id="form-submit" action="find_ticket" method="get">
                     <div class="row">
@@ -16,7 +39,7 @@
                                 <select required name='departure_city' id="nereden">
                                     <option value="">Şehir Seçiniz:</option>
                                     <?php foreach ($cities as $city) : ?>
-                                        <Option value="<?= $city['id']?>"><?= $city['city_name'] ?></Option>
+                                        <Option value="<?= $city['id'] ?>"><?= $city['city_name'] ?></Option>
                                     <?php endforeach; ?>
                                 </select>
                             </fieldset>
@@ -27,7 +50,7 @@
                                 <select required name='destination_city' id="nereye">
                                     <option value="">Şehir Seçiniz:</option>
                                     <?php foreach ($cities as $city) : ?>
-                                        <Option value="<?= $city['id']?>"><?= $city['city_name'] ?></Option>
+                                        <Option value="<?= $city['id'] ?>"><?= $city['city_name'] ?></Option>
                                     <?php endforeach; ?>
                                 </select>
                             </fieldset>
--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -223,22 +223,3 @@
       }
     });
   })();
-
-function PopUp(btn_id, PopUpid) {
-  document
-    .querySelector(`#${btn_id}`)
-    .addEventListener("click", function () {
-      document.querySelector(`#${PopUpid}`).classList.add("active");
-      document.querySelector(`#${PopUpid}`).querySelector('.popup').classList.add("active");
-    });
-
-  //Close Login Popup
-  document
-    .querySelector(`#${PopUpid} .close-btn`)
-    .addEventListener("click", function () {
-      document.querySelector(`#${PopUpid}`).classList.remove("active");
-      document.querySelector(`#${PopUpid}`).querySelector('.popup').classList.remove("active");
-    });
-}
-
-PopUp('pnr-control', 'overlay-pnr');
