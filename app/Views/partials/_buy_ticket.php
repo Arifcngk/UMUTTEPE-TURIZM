2f6e561a7e38c62d74ad3673f219648c5a41264d
Arifcngk <93188117+Arifcngk@users.noreply.github.com>
2024-03-06 23:26:21 +0300

    pnr update

--- app/Views/partials/_buy_ticket.php
+++ app/Views/partials/_buy_ticket.php
@@ -9,26 +9,27 @@
             <div class="submit-form">
                 <div class="btn-group pt-2 pb-2" role="group" aria-label="Route type switch">
                     <button type="button" class="btn btn-secondary" onclick="activateButton(1)">Tek Yön</button>
-                    <button type="button" class="btn btn-secondary active" onclick="activateButton(2)">Gidiş-Dönüş</button>
+                    <button type="button" class="btn btn-secondary active"
+                        onclick="activateButton(2)">Gidiş-Dönüş</button>
                 </div>
 
                 <script>
-                    function activateButton(buttonNumber) {
-                        // Tüm butonları pasif hale getir
-                        var buttons = document.querySelectorAll('.btn-group .btn');
-                        buttons.forEach(function(button) {
-                            button.classList.remove('active');
-                        });
+                function activateButton(buttonNumber) {
+                    // Tüm butonları pasif hale getir
+                    var buttons = document.querySelectorAll('.btn-group .btn');
+                    buttons.forEach(function(button) {
+                        button.classList.remove('active');
+                    });
 
-                        // Tıklanan butonu aktif hale getir
-                        var clickedButton = document.querySelector('.btn-group .btn:nth-child(' + buttonNumber + ')');
-                        clickedButton.classList.add('active');
+                    // Tıklanan butonu aktif hale getir
+                    var clickedButton = document.querySelector('.btn-group .btn:nth-child(' + buttonNumber + ')');
+                    clickedButton.classList.add('active');
 
 
-                        var returnDate = document.querySelector('#return');
+                    var returnDate = document.querySelector('#return');
 
-                        returnDate.disabled = (buttonNumber == 1) ? true : false; 
-                    }
+                    returnDate.disabled = (buttonNumber == 1) ? true : false;
+                }
                 </script>
                 <h4>Sefer Bilgilerinizi Giriniz</h4>
                 <form id="form-submit" action="find_ticket" method="get">
@@ -39,7 +40,7 @@
                                 <select required name='departure_city' id="nereden">
                                     <option value="">Şehir Seçiniz:</option>
                                     <?php foreach ($cities as $city) : ?>
-                                        <Option value="<?= $city['city_plate'] ?>"><?= $city['city_name'] ?></Option>
+                                    <Option value="<?= $city['city_plate'] ?>"><?= $city['city_name'] ?></Option>
                                     <?php endforeach; ?>
                                 </select>
                             </fieldset>
@@ -50,7 +51,7 @@
                                 <select required name='destination_city' id="nereye">
                                     <option value="">Şehir Seçiniz:</option>
                                     <?php foreach ($cities as $city) : ?>
-                                        <Option value="<?= $city['city_plate'] ?>"><?= $city['city_name'] ?></Option>
+                                    <Option value="<?= $city['city_plate'] ?>"><?= $city['city_name'] ?></Option>
                                     <?php endforeach; ?>
                                 </select>
                             </fieldset>
@@ -58,13 +59,15 @@
                         <div class="col-md-6">
                             <fieldset>
                                 <label for="departure">Gidiş Tarihi:</label>
-                                <input name="start_time" type="date" class="form-control date" id="departure" placeholder="Gidiş Tarihi...">
+                                <input name="start_time" type="date" class="form-control date" id="departure"
+                                    placeholder="Gidiş Tarihi...">
                             </fieldset>
                         </div>
                         <div class="col-md-6">
                             <fieldset>
                                 <label for="return">Dönüş Tarihi:</label>
-                                <input name="return" type="date" class="form-control date" id="return" placeholder="Dönüş Tarihi...">
+                                <input name="return" type="date" class="form-control date" id="return"
+                                    placeholder="Dönüş Tarihi...">
                             </fieldset>
                         </div>
                         <fieldset>
@@ -72,6 +75,7 @@
                         </fieldset>
                     </div>
                 </form>
+              
             </div>
         </div>
     </section>
--- app/Views/partials/_pnr_control.php
+++ app/Views/partials/_pnr_control.php
@@ -1,7 +1,7 @@
 <div class="overlay" id="overlay-pnr">
     <div class="popup" id="popup">
         <button class="close-btn" id="close-btn">X</button>
-        <form class="form" action="pnrControl" method="POST" style="width: 300px; height: 350px;">
+        <form class="form" action="pnrControl" method="POST" style="width: 400px; height: 250px;">
             <h2>PNR Numaranızı Giriniz</h2>
             <div class="form-element">
                 <label for="text">PNR:</label>
