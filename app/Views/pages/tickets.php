6c730d3d6925041b9c606bcb7472ee36c2620f5f
Mehmet Sezer <mehmetszr05@gmail.com>
2024-03-07 00:17:50 +0300

    Tickets page has been started

--- app/Views/pages/tickets.php
+++ app/Views/pages/tickets.php
@@ -22,22 +22,38 @@
 
     <main id="main">
         <div class="container">
-            <div class="search-form-wrapper">
-                <form action="" class="trn-search-bar">
-                    <div class="inner-container">
-                        <div class="search-form-container">
-                            <div class="search-form">
-                                <div class="panel-left">
-                                    
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8">
+                        <form class="search-form trn-search-bar" action="/otobus-bileti/otobus-arama" method="post" id="bus-trip-search-form">
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <input type="text" class="form-control" name="bus_trip_origin" value="İzmit (Kocaeli) - (İzmit (Kocaeli))" placeholder="Nereden?" readonly>
+                                    <input type="hidden" name="bus_trip_origin_slug" value="kocaeli-izmit">
                                 </div>
-                                <div class="panel-right">
-
+                                <div class="col-md-6 mb-3">
+                                    <input type="text" class="form-control" name="bus_trip_destination" value="İstanbul Avrupa - (İstanbul)" placeholder="Nereye?" readonly>
+                                    <input type="hidden" name="bus_trip_destination_slug" value="istanbul-avrupa-bolgesi">
                                 </div>
                             </div>
-                            <button class="btn btn-primary">Ara</button>
-                        </div>
+                            <div class="row">
+                                <div class="col-md-6 mb-3">
+                                    <input type="text" class="form-control" name="fromdate" readonly placeholder="Gidiş Tarihi">
+                                    <input type="hidden" name="bus_trip_departure_date" value="2024-03-07">
+                                </div>
+                                <div class="col-md-6 mb-3">
+                                    <button class="btn btn-outline-secondary btn-block" type="button">Bugün</button>
+                                    <button class="btn btn-outline-secondary btn-block" type="button">Yarın</button>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 mb-3">
+                                    <button type="button" class="btn btn-primary btn-block" id="btnBusTripSearch">Ara</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                </form>
+                </div>
             </div>
         </div>
     </main>
