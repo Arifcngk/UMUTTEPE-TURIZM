d2bef42ca9ec4088c6ad11fb80fa14cefc8939ec
Mehmet Sezer <mehmetszr05@gmail.com>
2024-03-29 06:15:36 +0300

    Update about

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -4,7 +4,7 @@
 <head>
     <!-- head başlangıc -->
 
-    <?php include "app/Views/partials/_head.php"?>
+    <?php include "app/Views/partials/_head.php" ?>
 
     <!-- head son -->
 </head>
@@ -56,42 +56,38 @@
                             <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
                                 <div class="count-box">
                                     <i class="bi bi-emoji-smile" style="color: #20b38e;"></i>
-                                    <span data-purecounter-start="0" data-purecounter-end="232"
-                                        data-purecounter-duration="1" class="purecounter"></span>
-                                    <p><strong>Mutlu Müşteri </strong> consequuntur voluptas nostrum aliquid ipsam
-                                        architecto ut.</p>
+                                    <span data-purecounter-start="0" data-purecounter-end="232" data-purecounter-duration="1" class="purecounter"></span>
+                                    <p><strong>Mutlu Müşteri </strong>iletişimde olduğumuz müşterilerimizden biri. Uzman ekibimiz, onların beklentilerini anlamak ve memnuniyetlerini sağlamak için çaba gösteriyor.</p>
                                 </div>
                             </div>
+
 
                             <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
                                 <div class="count-box">
                                     <i class="bi bi-emoji-smile" style="color: #8a1ac2;"></i>
-                                    <span data-purecounter-start="0" data-purecounter-end="521"
-                                        data-purecounter-duration="1" class="purecounter"></span>
-                                    <p><strong>Sefer</strong> adipisci atque cum quia aspernatur totam laudantium
-                                        et quia dere tan</p>
+                                    <span data-purecounter-start="0" data-purecounter-end="521" data-purecounter-duration="1" class="purecounter"></span>
+                                    <p><strong>Sefer</strong>, şimdiye kadar gerçekleştirdiğimiz toplam sefer sayısıdır. Müşterilerimizi güvenli ve konforlu bir şekilde hedeflerine ulaştırmak için yoğun gayret sarf ediyoruz.</p>
                                 </div>
                             </div>
+
 
                             <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
                                 <div class="count-box">
                                     <i class="bi bi-clock" style="color: #2cbdee;"></i>
-                                    <span data-purecounter-start="0" data-purecounter-end="18"
-                                        data-purecounter-duration="1" class="purecounter"></span>
-                                    <p><strong>Hızlı Satın Alma</strong> aut commodi quaerat modi aliquam nam
-                                        ducimus aut voluptate non vel</p>
+                                    <span data-purecounter-start="0" data-purecounter-end="18" data-purecounter-duration="1" class="purecounter"></span>
+                                    <p><strong>Hızlı Satın Alma</strong>, işlemi hızlandırmak için tasarladığımız bir hizmettir. Müşterilerimiz, kolay ve hızlı bir şekilde istedikleri seferi satın alabilir ve keyifli bir yolculuk geçirebilirler.</p>
                                 </div>
                             </div>
+
 
                             <div class="col-md-6 mt-5 d-md-flex align-items-md-stretch">
                                 <div class="count-box">
                                     <i class="bi bi-award" style="color: #ffb459;"></i>
-                                    <span data-purecounter-start="0" data-purecounter-end="16"
-                                        data-purecounter-duration="1" class="purecounter"></span>
-                                    <p><strong>Ödül</strong> rerum asperiores dolor alias quo reprehenderit eum et
-                                        nemo pad der</p>
+                                    <span data-purecounter-start="0" data-purecounter-end="16" data-purecounter-duration="1" class="purecounter"></span>
+                                    <p><strong>Ödül</strong>, çalışmalarımızın ve hizmet kalitemizin bir tezahürüdür. Müşteri memnuniyeti odaklı yaklaşımımız ve sürekli gelişim çabalarımız sayesinde elde ettiğimiz başarılarla gurur duyuyoruz.</p>
                                 </div>
                             </div>
+
                         </div>
                     </div><!-- End .content-->
 
@@ -99,24 +95,21 @@
                         <div class="progress">
                             <span class="skill">Öğrenci <i class="val">758</i></span>
                             <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0"
-                                    aria-valuemax="100"></div>
+                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
 
                         <div class="progress">
                             <span class="skill">Eğitimci <i class="val">289</i></span>
                             <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0"
-                                    aria-valuemax="100"></div>
+                                <div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
 
                         <div class="progress">
                             <span class="skill">Diğer <i class="val">958</i></span>
                             <div class="progress-bar-wrap">
-                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0"
-                                    aria-valuemax="100"></div>
+                                <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
                             </div>
                         </div>
                     </div>
@@ -156,52 +149,43 @@
                         <h4>Şehirler Arası İlk Sefer </h4>
                         <h5>2015 - 2016</h5>
                         <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                        <p>Qui deserunt veniam. Et sed aliquam labore tempore sed quisquam iusto autem sit. Ea vero
-                            voluptatum qui ut dignissimos deleniti nerada porti sand markend</p>
+                        <p>2015-2016 yılları arasında gerçekleştirdiğimiz ilk şehirler arası sefer, İstanbul'dan Ankara'ya yapılan bir seferdi. Bu sefer, müşterilerimize sunduğumuz hizmet yelpazesini genişletme ve daha fazla kitleye ulaşma fırsatı sağladı.</p>
                     </div>
+
                     <div class="resume-item">
                         <h4>İlk Kurumsal Kimlik</h4>
                         <h5>2010 - 2014</h5>
-                        <p><em>Rochester Institute of Technology, Rochester, NY</em></p>
-                        <p>Quia nobis sequi est occaecati aut. Repudiandae et iusto quae reiciendis et quis Eius vel
-                            ratione eius unde vitae rerum voluptates asperiores voluptatem Earum molestiae consequatur
-                            neque etlon sader mart dila</p>
+                        <p><em>İstanbul Teknik Üniversitesi, İstanbul, Türkiye</em></p>
+                        <p>2010-2014 yılları arasında geliştirdiğimiz ilk kurumsal kimlik, İstanbul Teknik Üniversitesi iş birliğiyle hayata geçirildi. Yenilikçi ve özgün tasarımımız, müşterilerimize güven ve kalite sunma konusundaki taahhüdümüzü yansıtmaktadır.</p>
                     </div>
+
                 </div>
                 <div class="col-lg-6">
                     <h3 class="resume-title">Yılın Turizm Firması</h3>
                     <div class="resume-item">
-                        <h4>Ödüller İle Daha Büyüdük</h4>
-                        <h5>2019 - Turizm Ödülleri</h5>
-
-                        <p>
+                        <h4>Ödüllerimizle Büyüyoruz</h4>
+                        <h5>2019 Turizm Ödülleri</h5>
+                        <p>2019 yılında aldığımız turizm ödülleri, firmamızın başarısını ve müşteri memnuniyetini yansıtmaktadır. Bu ödüller, tasarım, iletişim ve üretim süreçlerindeki liderliğimizi ve kalitemizi belgelemektedir. Ödüllerimiz arasında şunlar bulunmaktadır:</p>
                         <ul>
-                            <li>Lead in the design, development, and implementation of the graphic, layout, and
-                                production communication materials</li>
-                            <li>Delegate tasks to the 7 members of the design team and provide counsel on all aspects of
-                                the project. </li>
-                            <li>Supervise the assessment of all graphic materials in order to ensure quality and
-                                accuracy of the design</li>
-                            <li>Oversee the efficient use of production project budgets ranging from $2,000 - $25,000
-                            </li>
+                            <li>Grafik, düzen ve iletişim malzemelerinin tasarımı, geliştirilmesi ve uygulanmasında liderlik</li>
+                            <li>Tasarım ekibinin 7 üyesine görevlerin atanması ve projenin tüm yönlerinde danışmanlık sağlanması</li>
+                            <li>Tüm grafik materyallerinin değerlendirilmesinin denetlenmesi ve tasarımın kalitesinin ve doğruluğunun sağlanması</li>
+                            <li>$2,000 - $25,000 arasında değişen üretim proje bütçelerinin verimli kullanımının denetlenmesi</li>
                         </ul>
-                        </p>
                     </div>
+
                     <div class="resume-item">
                         <h4>Öğrenci Dostu Ulaşım Hedefi</h4>
                         <h5>2017 - 2024</h5>
-                        <p><em>Stepping Stone Advertising, New York, NY</em></p>
-                        <p>
+                        <p>Şirketimiz, 2017-2024 yılları arasında "Öğrenci Dostu Ulaşım" hedefiyle hareket etmektedir. Bu süreçte, öğrencilerin ulaşımını kolaylaştırmak ve onların ihtiyaçlarına uygun çözümler sunmak için çeşitli projeler geliştirdik. İşte bu dönemde gerçekleştirdiğimiz bazı faaliyetler:</p>
                         <ul>
-                            <li>Developed numerous marketing programs (logos, brochures,infographics, presentations, and
-                                advertisements).</li>
-                            <li>Managed up to 5 projects or tasks at a given time while under pressure</li>
-                            <li>Recommended and consulted with clients on the most appropriate graphic design</li>
-                            <li>Created 4+ design presentations and proposals a month for clients and account managers
-                            </li>
+                            <li>Çeşitli pazarlama programları geliştirildi (logolar, broşürler, infografikler, sunumlar ve reklamlar).</li>
+                            <li>Zorlu koşullarda bile aynı anda 5 projeyi veya görevi yönetme</li>
+                            <li>Müşterilere en uygun grafik tasarımı hakkında önerilerde bulunma ve danışmanlık yapma</li>
+                            <li>Ayda 4'ten fazla tasarım sunumu ve teklif oluşturma</li>
                         </ul>
-                        </p>
                     </div>
+
                 </div>
             </div>
 
@@ -212,11 +196,10 @@
 
 
     <!-- ======= Footer ======= -->
-    <?php include "app/Views/partials/_footer.php"?>
+    <?php include "app/Views/partials/_footer.php" ?>
     <!--  Footer-Son -->
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
     <script src="public/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
